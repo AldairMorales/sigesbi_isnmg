e70c3e08c5080d01eff6f0e3198d73c0d5ea44f2
AldairMorales <aldairmorales@protonamil.com>
2021-11-28 19:52:22 -0500

    requirements and unicorn

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ import pymysql
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:amg123456@127.0.0.1:5432/biblioteca"
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://localhost\SQLEXPRESS:@127.0.0.1:5000/biblioteca"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
